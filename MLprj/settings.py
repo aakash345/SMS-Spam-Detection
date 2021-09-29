2a9f16d1edc8440760da8975bee68cb497d66766
Aakash Gupta <aakagupta375@gmail.com>
2021-09-29 11:37:04 +0530

    third commit

--- MLprj/settings.py
+++ MLprj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')hl1s4y-lamz!y0_wjte@g=-n5amjs*uh$b10lz!-eahv786w0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn MLprj.wsgi --log-file -
